a4b7be145a83b4358813234f093f8b95c277ae88
Eric Brelsford <ebrelsford@gmail.com>
2011-05-17 13:35:26 -0400

    gracefully handle 403

--- barn/barn/settings.example.py
+++ barn/barn/settings.example.py
@@ -82,6 +82,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+
+    'middleware.http.Http403Middleware',
 )
 
 ROOT_URLCONF = 'barn.urls'
