6112a5d8d86865116ca17f855f6d70fde056663c
Eric Brelsford <ebrelsford@gmail.com>
2011-05-19 15:32:25 -0400

    missed context processors

--- barn/barn/settings.example.py
+++ barn/barn/settings.example.py
@@ -76,6 +76,14 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    
+    'context_processors.garden_types',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
