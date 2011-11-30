d41437e37ed79c780e3824fb64149c013f6c426e
Eric Brelsford <ebrelsford@gmail.com>
2011-11-29 19:17:48 -0500

    make estimated yields via admin

---
+++ barn/barn/estimates/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.conf.urls.defaults import patterns, url
+
+from estimates.models import EstimatedYield, EstimatedCost
+from estimates.admin_views import make_yield_estimates
+
+class EstimatedYieldAdmin(admin.ModelAdmin):
+    search_fields = ('variety__name',)
+    list_display = ('variety', 'estimated', 'pounds_per_plant', 'notes', 'should_be_used')
+    list_filter = ('estimated', 'should_be_used', 'variety')
+
+    def make_yield_estimates_success(self, request):
+        """Message user, redirect back to list"""
+        self.message_user(request, "Successfully created new Estimated yields.")
+        opts = self.model._meta
+        return redirect('admin:%s_%s_changelist' % (opts.app_label, opts.object_name.lower()))
+
+    def get_urls(self):
+        opts = self.model._meta
+        app_label, object_name = (opts.app_label, opts.object_name.lower())
+        prefix = "%s_%s" % (app_label, object_name)
+
+        urls = super(EstimatedYieldAdmin, self).get_urls()
+        my_urls = patterns('',
+            url(r'^makeall/$', make_yield_estimates, name='%s_makeall' % prefix),
+            url(r'^makeall/success/$', self.make_yield_estimates_success, name='%s_makeall_success' % prefix),
+        )
+        return my_urls + urls
+
+class EstimatedCostAdmin(admin.ModelAdmin):
+    search_fields = ('variety__name',)
+    list_display = ('variety', 'estimated', 'cost_per_pound', 'source', 'notes', 'organic', 'should_be_used')
+    list_filter = ('estimated', 'source', 'should_be_used', 'variety')
+
+admin.site.register(EstimatedYield, EstimatedYieldAdmin)
+admin.site.register(EstimatedCost, EstimatedCostAdmin)
---
+++ barn/barn/estimates/make_estimates.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from django.db.models import Max, Sum
+
+from farmingconcrete.models import Variety
+from harvestcount.models import Harvest
+from models import EstimatedYield
+
+def make_all_yield_estimates(start, end, by_gardener=True):
+    varieties = Variety.objects.filter(harvest__harvested__gte=start, harvest__harvested__lt=end).distinct()
+    for variety in varieties:
+        pounds_per_plant = _find_yield_estimate(variety, start, end, by_gardener)
+        estimated_yield = EstimatedYield(
+            variety=variety, 
+            estimated=datetime.now(), 
+            notes="added via admin", 
+            should_be_used=True, 
+            pounds_per_plant=str(pounds_per_plant)
+        )
+        estimated_yield.save()
+
+def _find_yield_estimate(variety, start, end, _by_gardener=True):
+    """Find an estiated yield for the given variety using harvests recorded in the given date range"""
+
+    # get total weight, maximum plants by gardener
+    results = Harvest.objects.filter(variety=variety, harvested__gte=start, harvested__lt=end).values('gardener').annotate(pounds=Sum('weight'), plants=Max('plants'))
+    averages = []
+
+    results = [r for r in results if r['pounds'] and r['plants']]
+
+    for result in results:
+        pounds = result['pounds']
+        plants = result['plants']
+        average = float(pounds) / float(plants)
+        averages.append(average)
+
+    total_plants = sum([float(result['plants']) for result in results])
+
+    if _by_gardener and averages:
+        average = sum(averages) / len(averages)
+    elif not _by_gardener and total_plants:
+        total_pounds = sum([float(result['pounds']) for result in results])
+        average = total_pounds / total_plants
+    else:
+        average = 0
+
+    return average
