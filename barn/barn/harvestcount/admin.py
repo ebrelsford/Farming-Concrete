4b312b35ce9edcd1bf4fd773c14581df6f8f3278
Eric Brelsford <ebrelsford@gmail.com>
2012-04-04 17:45:18 -0400

    more search fields on harvest

--- barn/barn/harvestcount/admin.py
+++ barn/barn/harvestcount/admin.py
@@ -10,7 +10,7 @@ class HarvestAdmin(admin.ModelAdmin):
     fields = ('variety', 'weight', 'plants',)
     list_display = ('variety', 'weight', 'plants', 'gardener', 'harvested',)
     list_filter = ('harvested', 'variety__name',)
-    search_fields = ('variety__name',)
+    search_fields = ('variety__name', 'gardener__name', 'gardener__garden__name',)
 
 admin.site.register(Gardener, GardenerAdmin)
 admin.site.register(Harvest, HarvestAdmin)
