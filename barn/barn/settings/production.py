c0da021837fc845ca4e936eef1dc4ffb90ebec41
Eric Brelsford <ebrelsford@gmail.com>
2013-08-04 14:18:55 -0400

    ALLOWED_HOSTS

--- barn/barn/settings/production.py
+++ barn/barn/settings/production.py
@@ -7,3 +7,7 @@ TEMPLATE_DEBUG = False
 EMAIL_HOST = get_env_variable('FARMING_CONCRETE_EMAIL_HOST')
 EMAIL_HOST_USER = get_env_variable('FARMING_CONCRETE_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_env_variable('FARMING_CONCRETE_EMAIL_HOST_PASSWORD')
+
+ALLOWED_HOSTS = [
+    '.farmingconcrete.org',
+]
