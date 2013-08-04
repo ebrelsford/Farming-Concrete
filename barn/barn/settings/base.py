5e5f492180121dddcd7f364c062cc6f2b56106f2
Eric Brelsford <ebrelsford@gmail.com>
2013-08-04 14:28:46 -0400

    Logging settings

--- barn/barn/settings/base.py
+++ barn/barn/settings/base.py
@@ -125,3 +125,27 @@ AJAX_LOOKUP_CHANNELS = {
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
 FARMINGCONCRETE_YEAR = 2013
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
--- barn/barn/settings/local.py
+++ barn/barn/settings/local.py
@@ -1,1 +1,28 @@
 from .base import *
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(PROJECT_ROOT, 'logs', 'django.log'),
+            'maxBytes': '16777216', # 16megabytes
+            'formatter': 'verbose'
+        },
+    },
+    'root': {
+        'handlers': ['log_file',],
+        'level': 'DEBUG',
+    },
+}
