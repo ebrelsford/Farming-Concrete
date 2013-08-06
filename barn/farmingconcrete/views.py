ef57e018dec08c0b54f00782607df8bc37fe3454
Eric Brelsford <ebrelsford@gmail.com>
2013-08-06 15:51:34 -0400

    Unused import

--- barn/farmingconcrete/views.py
+++ barn/farmingconcrete/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.db.models import Sum, Q
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.views.generic import CreateView, DetailView, View, TemplateView
