7b15302a2e66001c5ecc969a0d4c5acf00921a8c
Eric Brelsford <ebrelsford@gmail.com>
2013-08-12 15:39:24 -0400

    All cropcount urls in cropcount

--- barn/metrics/cropcount/urls.py
+++ barn/metrics/cropcount/urls.py
@@ -9,27 +9,72 @@ urlpatterns = patterns('',
         name='cropcount_index'
     ),
 
-    url(r'^beds/(?P<id>\d+)/$',
+
+    # Garden lists
+
+    url(r'^yours/(?:(?P<year>\d{4})/)?$',
+        'metrics.cropcount.views.user_gardens',
+        name='cropcount_user_gardens'
+    ),
+
+    url(r'^counted/(?:(?P<year>\d{4})/)?$',
+        'metrics.cropcount.views.all_gardens',
+        name='cropcount_all_gardens'
+    ),
+
+
+    # Garden details
+
+    url(r'^gardens/(?P<id>\d+)/(?:(?P<year>\d{4})/)?$',
+        'metrics.cropcount.views.garden_details',
+        name='cropcount_garden_details'
+    ),
+
+    url(r'^gardens/(?P<id>\d+)/(?:(?P<year>\d{4})/)?summary/$',
+        'metrics.cropcount.views.summary',
+        name='cropcount_summary'
+    ),
+
+    url(r'^gardens/(?P<id>\d+)/(?:(?P<year>\d{4})/)?csv/$',
+        'metrics.cropcount.views.download_garden_cropcount_as_csv',
+        name='cropcount_download_garden_cropcount_as_csv'
+    ),
+
+
+    # Add
+
+    url(r'^gardens/(?P<id>\d+)/(?:(?P<year>\d{4})/)?beds/add/$',
+        'metrics.cropcount.views.add_bed',
+        name='cropcount_add_bed'
+    ),
+
+
+    # Beds
+
+    url(r'^beds/(?P<id>\d+)/(?:(?P<year>\d{4})/)?$',
         'metrics.cropcount.views.bed_details',
         name='cropcount_bed_details'
     ),
 
-    url(r'^beds/(?P<id>\d+)/delete/$',
+    url(r'^beds/(?P<id>\d+)/(?:(?P<year>\d{4})/)?delete/$',
         'metrics.cropcount.views.delete_bed',
         name='cropcount_delete_bed'
     ),
 
-    url(r'^beds/(?P<bed_id>\d+)/patches/add/',
+    url(r'^beds/(?P<bed_id>\d+)/(?:(?P<year>\d{4})/)?patches/add/',
         'metrics.cropcount.views.add_patch',
         name='cropcount_add_patch'
     ),
 
-    url(r'^patches/(?P<id>\d+)/confirm-delete/$',
+
+    # Patches
+
+    url(r'^patches/(?P<id>\d+)/(?:(?P<year>\d{4})/)?confirm-delete/$',
         ConfirmDeletePatchView.as_view(),
         name='cropcount_patches_confirm_delete',
     ),
 
-    url(r'^patches/(?P<id>\d+)/delete/$',
+    url(r'^patches/(?P<id>\d+)/(?:(?P<year>\d{4})/)?delete/$',
         'metrics.cropcount.views.delete_patch',
         name='cropcount_delete_patch'
     ),
