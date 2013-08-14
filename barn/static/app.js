9329ce4b939d743f4054bf9cecd217172fc52af3
Eric Brelsford <ebrelsford@gmail.com>
2013-08-13 20:22:13 -0400

    Modernizr, latest jQuery-ui

--- barn/static/app.js
+++ barn/static/app.js
@@ -7,9 +7,10 @@ requirejs.config({
         'jquery': '../lib/jquery-1.10.2.min',
         'jquery.form': '../lib/jquery.form',
         'jquery.spin': '../lib/jquery.spin',
-        'jqueryui': 'lib/jquery-ui-1.10.3.custom',
+        'jqueryui': '../lib/jquery-ui/js/jquery-ui-1.10.3.custom',
         'leaflet': '../lib/leaflet/leaflet-src',
         'leaflet.usermarker': '../lib/leaflet-usermarker/leaflet.usermarker',
+        'modernizr': '../lib/modernizr',
         'spin': '../lib/spin',
         'underscore': 'lib/underscore-min',
     },
@@ -20,10 +21,14 @@ requirejs.config({
             exports: 'Django',
         },
         'jquery.form': ['jquery'],
+        'jqueryui': ['jquery'],
         'handlebars': {
             exports: 'Handlebars',
         },
         'leaflet.usermarker': ['leaflet'],
+        'modernizr': {
+            exports: 'Modernizr',
+        },
         'underscore': {
             exports: '_',
         },
