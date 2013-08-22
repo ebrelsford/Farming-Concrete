23481e49d3fcd8a759759a53b3af94559968372b
Eric Brelsford <ebrelsford@gmail.com>
2013-08-22 16:49:57 -0400

    Force maximum of today on RecordedInput

--- barn/metrics/forms.py
+++ barn/metrics/forms.py
@@ -1,5 +1,16 @@
+from datetime import date
+
 from django.forms import DateInput
 
 
 class RecordedInput(DateInput):
     input_type = 'date'
+
+    def __init__(self, attrs=None, *args, **kwargs):
+        try:
+            if not attrs or not 'max' in attrs:
+                attrs = {}
+            attrs['max'] = date.today().isoformat()
+        except Exception:
+            pass
+        super(RecordedInput, self).__init__(attrs=attrs)
