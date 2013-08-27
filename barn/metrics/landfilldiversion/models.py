c6d2588d93bae750cc8a13c84d153672b3dc7d1b
Eric Brelsford <ebrelsford@gmail.com>
2013-08-27 14:57:53 -0400

    Compost group

--- barn/metrics/landfilldiversion/models.py
+++ barn/metrics/landfilldiversion/models.py
@@ -52,7 +52,7 @@ class LandfillDiversionVolume(BaseMetricRecord):
 register('Landfill Diversion by Weight', {
     'model': LandfillDiversionWeight,
     'garden_detail_url_name': 'landfilldiversion_weight_garden_details',
-    'group': 'Landfill Diversion',
+    'group': 'Compost',
     'index_url_name': 'landfilldiversion_weight_index',
     'summarize_template': 'metrics/landfilldiversion/weight/summarize.html',
 })
@@ -61,7 +61,7 @@ register('Landfill Diversion by Weight', {
 register('Landfill Diversion by Volume', {
     'model': LandfillDiversionVolume,
     'garden_detail_url_name': 'landfilldiversion_volume_garden_details',
-    'group': 'Landfill Diversion',
+    'group': 'Compost',
     'index_url_name': 'landfilldiversion_volume_index',
     'summarize_template': 'metrics/landfilldiversion/volume/summarize.html',
 })
