0ed79c5f46ca08eaaa2d113bd3c87888b0e5d06f
Eric Brelsford <ebrelsford@gmail.com>
2013-10-16 16:41:06 -0400

    Use johnny-cache

--- barn/barn/settings/production.py
+++ barn/barn/settings/production.py
@@ -11,3 +11,22 @@ EMAIL_HOST_PASSWORD = get_env_variable('FARMING_CONCRETE_EMAIL_HOST_PASSWORD')
 ALLOWED_HOSTS = [
     '.farmingconcrete.org',
 ]
+
+#
+# Caching
+#
+
+CACHES = {
+    'default' : dict(
+        BACKEND = 'johnny.backends.memcached.MemcachedCache',
+        LOCATION = ['unix:///tmp/memcached.sock'],
+        JOHNNY_CACHE = True,
+    )
+}
+
+JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_myproj'
+
+MIDDLEWARE_CLASSES = (
+    'johnny.middleware.LocalStoreClearMiddleware',
+    'johnny.middleware.QueryCacheMiddleware',
+) + MIDDLEWARE_CLASSES
