c81a246287eafe5e17f0cf6f7fb7477645907d0d
Eric Brelsford <ebrelsford@gmail.com>
2013-10-23 12:11:37 -0400

    Ensure garden pk is copied to new formsets

--- barn/static/js/addparticipationhourspage.js
+++ barn/static/js/addparticipationhourspage.js
@@ -15,6 +15,10 @@ define(
             $('.projecthours-formset').formset({
                 addCssClass: 'add-row col-lg-6 col-lg-offset-4',
                 addText: 'Add another participant',
+                added: function (row) {
+                    var gardenPk = $('.projecthours-formset :input[name$=garden]:eq(0)').val();
+                    row.find(':input[name$=garden]').val(gardenPk);
+                }
             });
         });
 
