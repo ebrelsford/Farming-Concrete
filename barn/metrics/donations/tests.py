845fb0eaa00d025bf8a53518132b228ab41a0ccf
Eric Brelsford <ebrelsford@gmail.com>
2014-05-16 20:30:46 -0400

    Fix url name

--- barn/metrics/donations/tests.py
+++ barn/metrics/donations/tests.py
@@ -6,6 +6,6 @@ from ..tests import MetricTest
 class DonationsViewsTest(MetricTest):
 
     def test_garden_details(self):
-        resp = self.client.get(reverse('donation_garden_details',
+        resp = self.client.get(reverse('donations_garden_details',
                                        kwargs=self.get_garden_details_kwargs()))
         self.assertEqual(resp.status_code, 200)
