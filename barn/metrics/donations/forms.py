70f616eadf2fcfd3f5fc4e600bf1ade17f61a666
Eric Brelsford <ebrelsford@gmail.com>
2014-06-26 10:45:08 -0400

    Style donations

--- barn/metrics/donations/forms.py
+++ barn/metrics/donations/forms.py
@@ -1,23 +1,10 @@
-from django.contrib.auth.models import User
-from django.forms import HiddenInput, ModelChoiceField, ModelForm
-
-from farmingconcrete.models import Garden
-from ..forms import RecordedInput
+from ..forms import RecordForm, RecordedField
 from .models import Donation
 
 
-class DonationForm(ModelForm):
-    garden = ModelChoiceField(label='garden', queryset=Garden.objects.all(),
-                              widget=HiddenInput())
-    added_by = ModelChoiceField(
-        label='added_by',
-        queryset=User.objects.all(),
-        widget=HiddenInput()
-    )
+class DonationForm(RecordForm):
+    recorded = RecordedField(label='Date')
 
     class Meta:
         model = Donation
         fields = ('produce_name', 'pounds', 'recorded', 'added_by', 'garden',)
-        widgets = {
-            'recorded': RecordedInput(),
-        }
