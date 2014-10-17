82286ff3af986eb22fb33f2b2f7be6e091d6e5da
Eric Brelsford <ebrelsford@gmail.com>
2014-10-17 12:01:39 -0400

    Consolidate similarities in bar charts

--- barn/metrics/charts.py
+++ barn/metrics/charts.py
@@ -16,43 +16,21 @@ def make_chart_name(metric_name, garden):
     ])
 
 
-def horizontal_bar(data_frame, destination_file, color='#849F38', xlabel='',
-                   ylabel=''):
-    data_frame.plot(kind='barh', color=color, linewidth=0)
-
-    ax = plt.gca()
-
-    # Turn off y-axis grid
-    ax.yaxis.grid(False)
-
-    # Set axis labels
-    ax.set_ylabel(ylabel)
-    ax.set_xlabel(xlabel, fontsize=12, fontweight='bold')
+def _format_ticks(axis):
+    # Remove top, right ticks
+    axis.spines['top'].set_visible(False)
+    axis.spines['right'].set_visible(False)
 
     # Remove top, right ticks
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-
-    # Remove top, right ticks
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    axis.xaxis.set_ticks_position('bottom')
+    axis.yaxis.set_ticks_position('left')
 
     # Make ticks face out
-    ax.xaxis.set_tick_params(direction='out')
-    ax.yaxis.set_tick_params(direction='out', labelsize=14)
+    axis.xaxis.set_tick_params(direction='out')
+    axis.yaxis.set_tick_params(direction='out', labelsize=14)
 
-    # Add labels to rectangles
-    for rect in ax.get_children():
-        if not isinstance(rect, Rectangle):
-            continue
-        if rect.get_facecolor() == (1, 1, 1, 1):
-            continue
-        width = rect.get_width()
-        ax.text(width - (data_frame.max() * 0.05),
-                rect.get_y() + rect.get_height() / 2. - .03,
-                '%d' % int(width), ha='center', va='bottom', color='white',
-                fontweight='bold')
 
+def _save_chart(destination_file):
     # Do our best to get an appropriate file name and make room for it
     img_file = destination_file
     if not img_file.endswith('.png'):
@@ -68,6 +46,36 @@ def horizontal_bar(data_frame, destination_file, color='#849F38', xlabel='',
     plt.clf()
     plt.cla()
     return os.path.join('charts', img_file)
+
+
+def horizontal_bar(data_frame, destination_file, color='#849F38', xlabel='',
+                   ylabel=''):
+    data_frame.plot(kind='barh', color=color, linewidth=0)
+
+    ax = plt.gca()
+
+    # Turn off y-axis grid
+    ax.yaxis.grid(False)
+
+    # Set axis labels
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel, fontsize=12, fontweight='bold')
+
+    _format_ticks(ax)
+
+    # Add labels to rectangles
+    for rect in ax.get_children():
+        if not isinstance(rect, Rectangle):
+            continue
+        if rect.get_facecolor() == (1, 1, 1, 1):
+            continue
+        width = rect.get_width()
+        ax.text(width - (data_frame.max() * 0.05),
+                rect.get_y() + rect.get_height() / 2. - .03,
+                '%d' % int(width), ha='center', va='bottom', color='white',
+                fontweight='bold')
+
+    return _save_chart(destination_file)
 
 
 def vertical_bar(data_frame, destination_file, color='#849F38', xlabel='',
@@ -83,17 +91,7 @@ def vertical_bar(data_frame, destination_file, color='#849F38', xlabel='',
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel, fontsize=12, fontweight='bold')
 
-    # Remove top, right ticks
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-
-    # Remove top, right ticks
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    # Make ticks face out
-    ax.xaxis.set_tick_params(direction='out')
-    ax.yaxis.set_tick_params(direction='out', labelsize=14)
+    _format_ticks(ax)
 
     # Add labels to rectangles
     for rect in ax.get_children():
@@ -107,18 +105,4 @@ def vertical_bar(data_frame, destination_file, color='#849F38', xlabel='',
                 '%d' % int(height), ha='center', va='bottom', color='white',
                 fontweight='bold')
 
-    # Do our best to get an appropriate file name and make room for it
-    img_file = destination_file
-    if not img_file.endswith('.png'):
-        img_file = '%s.png' % img_file
-    filename = os.path.abspath(os.path.join(settings.MEDIA_ROOT, 'charts', img_file))
-    if not os.path.exists(os.path.dirname(filename)):
-        os.makedirs(os.path.dirname(filename))
-
-    # Save
-    pylab.savefig(filename, bbox_inches='tight')
-
-    # Close this plot so we don't affect other plots
-    plt.clf()
-    plt.cla()
-    return os.path.join('charts', img_file)
+    return _save_chart(destination_file)
