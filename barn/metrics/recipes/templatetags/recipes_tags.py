e07e99df3c2ce2fae7f280aa13cabf06d5354daa
Eric Brelsford <ebrelsford@gmail.com>
2014-10-21 00:04:59 -0400

    Recipes chart

---
+++ barn/metrics/recipes/templatetags/recipes_tags.py
@@ -0,0 +1,37 @@
+from django import template
+
+from classytags.helpers import AsTag
+import pandas as pd
+
+from metrics.charts import horizontal_bar, make_chart_name
+from metrics.templatetags.metrics_tags import ChartMixin, MetricTotalTag
+from ..models import RecipeTally
+
+register = template.Library()
+
+
+class RecipesChart(ChartMixin, AsTag):
+
+    def get_metric_model(self):
+        return RecipeTally
+
+    def get_chart(self, records, garden):
+        df = pd.DataFrame.from_records(records.values('recipes_count', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['recipes_count']
+        return horizontal_bar(qdf, make_chart_name('recipes', garden),
+                              xlabel='TOTAL RECIPES')
+
+
+class RecipesTotal(MetricTotalTag):
+
+    def get_metric_model(self):
+        return RecipeTally
+
+    def get_sum_field(self):
+        return 'recipes_count'
+
+
+register.tag(RecipesChart)
+register.tag(RecipesTotal)
