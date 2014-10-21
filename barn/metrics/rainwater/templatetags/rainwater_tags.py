f92c52f24b953e66bbca5963c080812854bf0650
Eric Brelsford <ebrelsford@gmail.com>
2014-10-21 16:20:32 -0400

    Rainwater cumulative

--- barn/metrics/rainwater/templatetags/rainwater_tags.py
+++ barn/metrics/rainwater/templatetags/rainwater_tags.py
@@ -3,7 +3,7 @@ from django import template
 from classytags.helpers import AsTag
 import pandas as pd
 
-from metrics.charts import horizontal_bar, make_chart_name
+from metrics.charts import horizontal_bar, line_fill, make_chart_name
 from metrics.templatetags.metrics_tags import ChartMixin, MetricTotalTag
 from ..models import RainwaterHarvest
 
@@ -22,7 +22,22 @@ class RainwaterHarvestChart(ChartMixin, AsTag):
 
         qdf = df.groupby('recorded').sum()['volume']
         return horizontal_bar(qdf, make_chart_name('rainwater', garden),
-                              xlabel='GALLONS HARVESTED')
+                              xlabel='GALLONS HARVESTED', shape='short')
+
+
+class RainwaterHarvestLineChart(ChartMixin, AsTag):
+
+    def get_metric_model(self):
+        return RainwaterHarvest
+
+    def get_chart(self, records, garden):
+        records = records.filter(volume__isnull=False)
+        df = pd.DataFrame.from_records(records.values('volume', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['volume']
+        return line_fill(qdf.cumsum(), make_chart_name('rainwater_line', garden),
+                         shape='short')
 
 
 class RainwaterHarvestTotal(MetricTotalTag):
@@ -35,4 +50,5 @@ class RainwaterHarvestTotal(MetricTotalTag):
 
 
 register.tag(RainwaterHarvestChart)
+register.tag(RainwaterHarvestLineChart)
 register.tag(RainwaterHarvestTotal)
