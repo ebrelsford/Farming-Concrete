315da22b05f24c40f328948702b1be93c1258a45
Eric Brelsford <ebrelsford@gmail.com>
2014-10-20 23:53:15 -0400

    Donations charts

---
+++ barn/metrics/donations/templatetags/donations_tags.py
@@ -0,0 +1,36 @@
+from django import template
+
+from classytags.helpers import AsTag
+import pandas as pd
+
+from metrics.charts import vertical_bar, make_chart_name
+from metrics.templatetags.metrics_tags import ChartMixin, MetricTotalTag
+from ..models import Donation
+
+register = template.Library()
+
+
+class DonationsChart(ChartMixin, AsTag):
+    def get_metric_model(self):
+        return Donation
+
+    def get_chart(self, records, garden):
+        df = pd.DataFrame.from_records(records.values('pounds', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['pounds']
+        return vertical_bar(qdf, make_chart_name('donations', garden),
+                            ylabel='POUNDS DONATED')
+
+
+class DonationsTotal(MetricTotalTag):
+
+    def get_metric_model(self):
+        return Donation
+
+    def get_sum_field(self):
+        return 'pounds'
+
+
+register.tag(DonationsChart)
+register.tag(DonationsTotal)
