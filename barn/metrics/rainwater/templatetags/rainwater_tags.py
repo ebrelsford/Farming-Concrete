29266e9179bf46cc77992403a323b1540b17c183
Eric Brelsford <ebrelsford@gmail.com>
2014-10-20 23:27:51 -0400

    First shot at rainwater harvest

---
+++ barn/metrics/rainwater/templatetags/rainwater_tags.py
@@ -0,0 +1,38 @@
+from django import template
+
+from classytags.helpers import AsTag
+import pandas as pd
+
+from metrics.charts import horizontal_bar, make_chart_name
+from metrics.templatetags.metrics_tags import ChartMixin, MetricTotalTag
+from ..models import RainwaterHarvest
+
+register = template.Library()
+
+
+class RainwaterHarvestChart(ChartMixin, AsTag):
+
+    def get_metric_model(self):
+        return RainwaterHarvest
+
+    def get_chart(self, records, garden):
+        records = records.filter(volume__isnull=False)
+        df = pd.DataFrame.from_records(records.values('volume', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['volume']
+        return horizontal_bar(qdf, make_chart_name('rainwater', garden),
+                              xlabel='GALLONS HARVESTED')
+
+
+class RainwaterHarvestTotal(MetricTotalTag):
+
+    def get_metric_model(self):
+        return RainwaterHarvest
+
+    def get_sum_field(self):
+        return 'volume'
+
+
+register.tag(RainwaterHarvestChart)
+register.tag(RainwaterHarvestTotal)
