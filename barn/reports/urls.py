788222c3ed695e1d37eec9581b197492ce4500ac
Eric Brelsford <ebrelsford@gmail.com>
2014-10-22 00:40:02 -0400

    Remove view shortcut

--- barn/reports/urls.py
+++ barn/reports/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from .views import Index, ExportView, ReportView
 
 
-main_patterns = patterns('reports.views',
+main_patterns = patterns('',
     url(r'^(?P<year>\d+)?', Index.as_view(), name='reports_index'),
 )
 
-garden_patterns = patterns('reports.views',
+garden_patterns = patterns('',
     url(r'^export/', ExportView.as_view(), name='reports_export'),
     url(r'^(?P<year>\d{4})/pdf/', ReportView.as_view(), name='reports_pdf'),
 )
