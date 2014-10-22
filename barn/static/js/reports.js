32fdae1726ec0dc55cbe59a9080aef0d48952aa5
Eric Brelsford <ebrelsford@gmail.com>
2014-10-22 00:41:32 -0400

    Delete useless scripts from reports page

--- barn/static/js/reports.js
+++ barn/static/js/reports.js
@@ -4,64 +4,9 @@
 // Scripts dealing with the reports section of the Barn.
 //
 
-define(['jquery', 'leaflet.basicmap'], function ($) {
+define(['jquery'], function ($) {
 
     $(document).ready(function () {
-
-        $('.filter').change(function () {
-            function buildQueryString() {
-                var params = {};
-                $('select.filter').each(function () {
-                    var selection = $(this).find(':selected').val();
-                    if (selection === 'all') {
-                        return;
-                    }
-                    params[$(this).attr('name')] = selection;
-                });
-                $('.filter[type="checkbox"]:checked').each(function () {
-                    params[$(this).attr('name')] = 'yes';
-                });
-                return params;
-            }
-            var params = buildQueryString();
-            window.location = '/reports/?' + $.param(params);
-        });
-
-
-        // Show info-box
-
-        $('.average-yield').click(function (event) {
-            if (!$(this).text()) {
-                return;
-            }
-            var variety = $(this).data('crop-name');
-            var year = $(this).data('year');
-            $(this).addClass('picked');
-            $.get('{% url "estimates_estimatedyield_explain" %}?variety=' + variety + '&year=' + year, function (data) {
-                $('body').append($('<div/>')
-                    .html(data)
-                    .addClass('info-box')
-                    .position({
-                        my: 'left top',
-                        at: 'left top',
-                        of: event,
-                    })
-                );
-            });
-        });
-
-
-        // Hide info-box if anything outside of it is clicked
-
-        $(document).click(function (event) {
-            if ($(event.target).parents('.info-box').size()) {
-                return;
-            }
-            if ($('.info-box').size()) {
-                $('.average-yield').removeClass('picked');
-                $('.info-box').remove();
-            }
-        });
     });
 
 });
