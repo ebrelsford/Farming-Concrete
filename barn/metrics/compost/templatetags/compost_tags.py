e28903417e76fd949b003983990c7cbc3bf63915
Eric Brelsford <ebrelsford@gmail.com>
2014-11-03 00:40:07 -0500

    Compost titles and separators

--- barn/metrics/compost/templatetags/compost_tags.py
+++ barn/metrics/compost/templatetags/compost_tags.py
@@ -3,7 +3,7 @@ from django import template
 from classytags.helpers import AsTag
 import pandas as pd
 
-from metrics.charts import vertical_bar, make_chart_name
+from metrics.charts import vertical_bar, line_fill, make_chart_name
 from metrics.templatetags.metrics_tags import ChartMixin, MetricTotalTag
 from ..models import CompostProductionVolume, CompostProductionWeight
 
@@ -20,7 +20,21 @@ class CompostVolumeChart(ChartMixin, AsTag):
 
         qdf = df.groupby('recorded').sum()['volume']
         return vertical_bar(qdf, make_chart_name('compost_volume', garden),
-                            ylabel='GALLONS PRODUCED')
+                            ylabel='GALLONS', shape='short')
+
+
+class CompostVolumeLineChart(ChartMixin, AsTag):
+    def get_metric_model(self):
+        return CompostProductionVolume
+
+    def get_chart(self, records, garden):
+        df = pd.DataFrame.from_records(records.values('volume', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['volume']
+        return line_fill(qdf.cumsum(),
+                         make_chart_name('compost_volume_line', garden),
+                         ylabel='GALLONS', shape='short')
 
 
 class CompostVolumeTotal(MetricTotalTag):
@@ -42,7 +56,21 @@ class CompostWeightChart(ChartMixin, AsTag):
 
         qdf = df.groupby('recorded').sum()['weight']
         return vertical_bar(qdf, make_chart_name('compost_weight', garden),
-                            ylabel='POUNDS PRODUCED')
+                            ylabel='POUNDS', shape='short')
+
+
+class CompostWeightLineChart(ChartMixin, AsTag):
+    def get_metric_model(self):
+        return CompostProductionWeight
+
+    def get_chart(self, records, garden):
+        df = pd.DataFrame.from_records(records.values('weight', 'recorded'),
+                                       coerce_float=True)
+
+        qdf = df.groupby('recorded').sum()['weight']
+        return line_fill(qdf.cumsum(),
+                         make_chart_name('compost_weight_line', garden),
+                         ylabel='POUNDS', shape='short')
 
 
 class CompostWeightTotal(MetricTotalTag):
@@ -55,6 +83,8 @@ class CompostWeightTotal(MetricTotalTag):
 
 
 register.tag(CompostVolumeChart)
+register.tag(CompostVolumeLineChart)
 register.tag(CompostVolumeTotal)
 register.tag(CompostWeightChart)
+register.tag(CompostWeightLineChart)
 register.tag(CompostWeightTotal)
