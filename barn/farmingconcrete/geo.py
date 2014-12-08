c6c3fc97b1dcf8536dcbfa9baeb41ce4718eefc1
Eric Brelsford <ebrelsford@gmail.com>
2014-12-08 14:35:16 -0500

    Fix geo details

--- barn/farmingconcrete/geo.py
+++ barn/farmingconcrete/geo.py
@@ -1,4 +1,7 @@
 import geojson
+from pygeocoder import Geocoder
+
+from .models import Garden
 
 
 def garden_collection(gardens):
@@ -17,3 +20,33 @@ def garden_feature(garden):
             'name': garden.name,
         }
     )
+
+
+def fetch_geo_details(garden):
+    """Reverse geocode to get garden's state, city, etc"""
+    results = Geocoder.reverse_geocode(garden.latitude, garden.longitude)
+    address_components = results[0].data[0]['address_components']
+
+    for component in address_components:
+        # Borough
+        if 'sublocality' in component['types']:
+            borough = component['long_name']
+            if not garden.borough and borough in [b[0] for b in Garden.BOROUGH_CHOICES]:
+                garden.borough = component['long_name']
+
+        # City
+        if 'locality' in component['types']:
+            if not garden.city:
+                garden.city = component['long_name']
+
+        # State
+        if 'administrative_area_level_1' in component['types']:
+            if not garden.state:
+                garden.state = component['short_name']
+
+        # Zip
+        if 'postal_code' in component['types']:
+            if not garden.zip:
+                garden.zip = component['long_name']
+
+    garden.save()
