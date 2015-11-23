ce60d61f1f2792fca30d4be0d808d5ff2d5cd786
Eric Brelsford <ebrelsford@gmail.com>
2015-11-23 10:42:45 -0500

    Next and previous nav

--- barn/static/actions/index.js
+++ barn/static/actions/index.js
@@ -1,11 +1,14 @@
-function showActions(limit) {
+var limit = 5,
+    offset = 0;
+
+function showActions(limit, offset) {
     var list = document.querySelectorAll('.actions-list')[0];
     list.innerHTML = null;
 
     var template = document.getElementById('item-template').innerHTML,
         compiledTemplate = doT.template(template);
 
-    qwest.get('../api-admin/actions/?limit=' + limit)
+    qwest.get('../api-admin/actions/?limit=' + limit + '&offset=' + offset)
         .then(function (xhr, data) {
             data.results.forEach(function (action) {
                 var listItem = document.createElement('li');
@@ -17,15 +20,28 @@ function showActions(limit) {
 }
 
 function onReady() {
-    showActions(5);
+    showActions(limit);
 
+    // Initialize limit links
     var setLimitLinks = document.querySelectorAll('.set-limit');
     for (var i = 0; i < setLimitLinks.length; i++) {
         setLimitLinks[i].addEventListener('click', function (e) {
-            showActions(e.target.getAttribute('data-limit'))
+            showActions(e.target.getAttribute('data-limit'), offset)
             e.preventDefault();
         });
     }
+
+    // Initialize next and previous links
+    document.querySelectorAll('.actions-previous')[0].addEventListener('click', function (e) {
+        offset -= limit;
+        showActions(limit, offset);
+        e.preventDefault();
+    });
+    document.querySelectorAll('.actions-next')[0].addEventListener('click', function (e) {
+        offset += limit;
+        showActions(limit, offset);
+        e.preventDefault();
+    });
 }
 
 if (document.readyState !== 'loading') {
